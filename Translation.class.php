1d7011676ed496894cb47f747ae16e88267931c4
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2017-06-08 18:23:13 +0900

    New: Translation

---
+++ Translation.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Translation.class.php
+ *
+ * @creation  2017-06-08
+ * @version   1.0
+ * @package   unit-google
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** Translation
+ *
+ * @creation  2017-06-08
+ * @version   1.0
+ * @package   unit-google
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class Translation
+{
+	/** trait.
+	 *
+	 */
+	use OP_CORE;
+}
