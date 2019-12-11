61da9ff9d47f5264be922ccbb2cd734fd7e87fb1
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-12-11 13:00:48 +0900

    New: Translation - Language(), Translate()

--- Translation.class.php
+++ Translation.class.php
@@ -23,4 +23,229 @@ class Translation
 	 *
 	 */
 	use OP_CORE;
+
+	/** Google cloud platform's api key.
+	 *
+	 * @var string
+	 */
+	const _API_KEY_ = 'google-cloud-translation';
+
+	/** Where error messages are stored.
+	 *
+	 * @var array
+	 */
+	static private $_errors = [];
+
+	/** Stored of error messages.
+	 *
+	 * @param string $error
+	 */
+	static function _Error($error)
+	{
+		self::$_errors[] = $error;
+	}
+
+	/** Google cloud platform's api key.
+	 *
+	 * @param  string|null $apikey
+	 * @return string
+	 */
+	static function ApiKey($apikey=null)
+	{
+		if( $apikey ){
+			Env::Set(self::_API_KEY_, $apikey);
+		}
+		return Env::Get( self::_API_KEY_ );
+	}
+
+	/** Return error message.
+	 *
+	 * @return array
+	 */
+	static function Errors()
+	{
+		return self::$_errors;
+	}
+
+	/** Get supported language code list.
+	 *
+	 * <pre>
+	 * //  Set onepiece-frameworks unit directory.
+	 * Unit::SetDirectory('app:/app/unit');
+	 *
+	 * //  Load unit of google.
+	 * Unit::Load('google');
+	 *
+	 * //  Setup google cloud platform's api key.
+	 * Translation::ApiKey('your-api-key');
+	 *
+	 * //  Setup configuration.
+	 * $config = [];
+	 * $config['model']  = 'nmt'; // NMT=Neural Machine Translation, PBMT=Phrase-Based Machine Translation
+	 * $config['target'] = 'en';  // Language name will translate.
+	 * $config['array']  = true;  // true is return array, false is return assoc.
+	 *
+	 * //  Get supported language list.
+	 * $result = Translation::Language($config);
+	 * d($result);
+	 * </pre>
+	 *
+	 * @param  array $config
+	 * @return array
+	 */
+	static function Language($config)
+	{
+		//	...
+		if(!$apikey = self::ApiKey() ){
+			self::_Error("Has not been set google cloud platform's api key.");
+			return false;
+		}
+
+		//	...
+		$domain = 'translation.googleapis.com';
+		$path   = '/language/translate/v2/languages';
+		$url    = "https://{$domain}{$path}";
+
+		//	...
+		$model   = ifset($config['model'] , 'nmt');
+		$target  = ifset($config['target'], 'en' );
+		$isarray = ifset($config['array'] , true ); // Return result format.
+
+		//	...
+		$post = array();
+		$post['key']	 = $apikey;
+		$post['model']	 = $model;
+		$post['target']	 = $target;
+
+		//	...
+		$text = Curl::Get($url, $post);
+		$json = json_decode($text, true);
+
+		//	...
+		$result = [];
+
+		//	...
+		if( isset($json['data']['languages']) ){
+			foreach( $json['data']['languages'] as $language ){
+				//	...
+				$code = $language['language'];
+				$name = $language['name'];
+
+				//	...
+				if( $isarray ){
+					//	Return result value is array.
+					$result[] = ['code'=>$code, 'name'=>$name];
+				}else{
+					//	Return result value is assoc. (object)
+					$result[$code] = $name;
+				}
+			}
+		}
+
+		//	...
+		if( isset($json['error']['errors']) ){
+			foreach( $json['error']['errors'] as $error ){
+				self::$_errors[] = $error['message'];
+			}
+
+		}
+
+		//	...
+		return $result;
+	}
+
+	/** Translate
+	 *
+	 * @param  array $config
+	 * @return array
+	 */
+	static function Translate($config)
+	{
+		//	...
+		if(!$apikey = self::ApiKey() ){
+			self::_Error("Has not been set google cloud platform's api key.");
+			return false;
+		}
+
+		//	...
+		$domain = 'translation.googleapis.com';
+		$path   = '/language/translate/v2';
+		$url    = "https://{$domain}{$path}";
+
+		//	...
+		$model   = ifset($config['model'],  'nmt');
+		$format  = ifset($config['format'], 'html');
+		$source  = ifset($config['source']);
+		$target  = ifset($config['target']);
+		$string  = ifset($config['string']);
+		$strings = ifset($config['strings']);
+
+		//	...
+		if(!$source){
+			self::_Error("Has not been set source language code.");
+		}
+
+		//	...
+		if(!$target){
+			self::_Error("Has not been set target language code.");
+		}
+
+		//	...
+		if( empty($string) and empty($strings) ){
+			self::_Error("Has not been set translate string.");
+		}
+
+		//	...
+		if( self::$_errors ){
+			return false;
+		}
+
+		//	...
+		$post = array();
+		$post['key']	 = $apikey;
+		$post['model']	 = $model;
+		$post['format']	 = $format;
+		$post['source']	 = $source;
+		$post['target']	 = $target;
+
+		//	...
+		$data = http_build_query($post, null, '&');
+
+		//	...
+		if( $string ){
+			$strings[] = $string;
+		}
+
+		//	...
+		if( $strings ){
+			$strings = Html::Decode($strings);
+			foreach( $strings as $string ){
+				$data .= '&q=' . Curl::Escape($string);
+			}
+		}
+
+		//	...
+		$text = Curl::Get($url.'?'.$data);
+		$json = json_decode($text, true);
+
+		//	...
+		$result = [];
+
+		//	...
+		if( isset($json['data']['translations']) ){
+			foreach( $json['data']['translations'] as $translation ){
+				$result[] = $translation['translatedText'];
+			}
+		}
+
+		//	...
+		if( isset($json['error']['errors']) ){
+			foreach( $json['error']['errors'] as $error ){
+				self::$_errors[] = $error['message'];
+			}
+		}
+
+		//	...
+		return $result;
+	}
 }
