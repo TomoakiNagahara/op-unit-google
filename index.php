898e333ddcbf1b725649a785171ca827c6ec54a7
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-12-11 13:33:20 +0900

    Del: index.php - spl_autoload_register is move to autoloader.php.

--- index.php
+++ index.php
@@ -1,36 +1,18 @@
 <?php
 /**
- * index.php
+ * unit-i18n:/index.php
  *
- * @creation  2017-06-08
+ * @creation  2018-02-15
  * @version   1.0
- * @package   unit-google
+ * @package   unit-i18n
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
-//	Setup auto loader.
-spl_autoload_register( function ($name){
-	try {
-		//	...
-		$namespace = 'OP\UNIT\GOOGLE\\';
+//	...
+//include('autoloader.php');
 
-		//	...
-		if( 0 !== $pos = strpos($name, $namespace)){
-			return;
-		}
+//	...
+include(__DIR__.'/Google.class.php');
 
-		//	...
-		$name = substr($name, strlen($namespace));
-
-		//	...
-		$path = __DIR__."/{$name}.class.php";
-
-		//	...
-		if( file_exists($path) ){
-			include($path);
-		}
-	} catch ( Throwable $e ){
-		Notice::Set($e);
-	}
-});
+//	...
 return true;
