1843e7a54397f0f272b4e775be4a9950637469be
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-12-11 13:36:21 +0900

    New: autoloader.php

---
+++ autoloader.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * unit-google:/autoloader.php
+ *
+ * @created   2018-02-16
+ * @version   1.0
+ * @package   unit-i18n
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+spl_autoload_register( function($name){
+	//	...
+	$unit = 'Google';
+	$UNIT = strtoupper($unit);
+
+	//	...
+	$namespace = "OP\UNIT\\{$UNIT}";
+
+	//	...
+	if( $name === "OP\UNIT\\{$UNIT}" ){
+		$name  =  $unit;
+	}else
+		if( strpos($name, $namespace) === 0 ){
+			$name = substr($name, strlen($namespace) +1);
+	}else{
+		return;
+	}
+
+	//	...
+	$path = __DIR__."/{$name}.class.php";
+
+	//	...
+	if( file_exists($path) ){
+		include($path);
+	}else{
+		Notice::Set("Does not exists this file. ($path)");
+	}
+});
