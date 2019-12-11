d4dc71105893b52e3c915c01bb4450bea1933d40
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-12-11 15:24:38 +0900

    Add: index.php - namespace

--- index.php
+++ index.php
@@ -2,14 +2,26 @@
 /**
  * unit-google:/index.php
  *
- * @creation  2017-06-08
+ * @created   2017-06-08
  * @version   1.0
  * @package   unit-google
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
+
+/** namespace
+ *
+ * @created
+ */
+namespace OP;
+
+/** Used class
+ *
+ * @created   2019-04-01
+ */
+
 //	...
-if(!\Unit::Load('curl')){
+if(!Unit::Load('curl')){
 	return false;
 }
 
